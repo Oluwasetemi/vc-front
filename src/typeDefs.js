70f73698c9605cca4c97232e846654638eaa1f1b
Ojo Oluwasetemi <setemiojo@gmail.com>
2021-01-09 17:25:17 +0100

    🐛 FIX: wip 3

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -72,64 +72,7 @@ const typeDefs = gql`
   }
 
   type StripeSubscription {
-  #   id: String
-  #   billing_cycle_anchor: 1610106475,
-  #   cancel_at: null,
-  #   cancel_at_period_end: false,
-  #   canceled_at: null,
-  #   collection_method: "charge_automatically",
-  #   created: 1610106475,
-  #   current_period_end: 1612784875,
-  #   current_period_start: 1610106475,
-  #   customer: "cus_IikxAfqeinhDF8",
-  #   days_until_due: null,
-  #   ended_at: null,
-
-  # "items": {
-  #   "object": "list",
-  #   "data": [
-  #     {
-  #       "id": "si_IikxmEj7BpdEVd",
-  #       "object": "subscription_item",
-  #       "billing_thresholds": null,
-  #       "created": 1610106476,
-  #       "metadata": {},
-  #       "price": {
-  #         "id": "price_1I77Hm2eZvKYlo2CxJp2c9fT",
-  #         "object": "price",
-  #         "active": true,
-  #         "billing_scheme": "per_unit",
-  #         "created": 1610059734,
-  #         "currency": "usd",
-  #         "livemode": false,
-  #         "lookup_key": null,
-  #         "metadata": {},
-  #         "nickname": null,
-  #         "product": "prod_IiYOMSw8RB6oKP",
-  #         "recurring": {
-  #           "aggregate_usage": null,
-  #           "interval": "month",
-  #           "interval_count": 1,
-  #           "usage_type": "licensed"
-  #         },
-  #         "tiers_mode": null,
-  #         "transform_quantity": null,
-  #         "type": "recurring",
-  #         "unit_amount": 1600,
-  #         "unit_amount_decimal": "1600"
-  #       },
-  #       "quantity": 1,
-  #       "subscription": "sub_Iikxie8L1VWrKw",
-  #       "tax_rates": []
-  #     }
-  #   ],
-  #   "has_more": false,
-  #   "url": "/v1/subscription_items?subscription=sub_Iikxie8L1VWrKw"
-  # },
-  # "livemode": false,
-  # "start_date": 1610106475,
-  # "status": "active",
-  # "transfer_data": null,
+    id: String
   }
 
   type SubscriptionService {
